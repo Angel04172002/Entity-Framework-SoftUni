0b01bd5895e3ee97b520dabce94932c8040f1e86
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-05-01 00:39:18 +0300

    Add files via upload

---
+++ 4. Entity Relations/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.CourseNameMaxLength)]
+        [Unicode]
+        public string Name { get; set; } = string.Empty;
+
+        [Unicode]
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Resource> Resources { get; set; }
+          = new List<Resource>();
+
+        public ICollection<Homework> Homeworks { get; set; }
+          = new List<Homework>();
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+         = new List<StudentCourse>();
+    }
+}
---
+++ 4. Entity Relations/StudentSystemContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+            
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> Homeworks { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if(optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer("");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(pk => new { pk.CourseId, pk.StudentId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
