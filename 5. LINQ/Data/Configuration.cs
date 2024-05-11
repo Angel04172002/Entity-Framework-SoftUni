564cc3c86fccb46e44b86ce5e070332923a443f2
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-05-11 21:18:36 +0300

    Delete 5. LINQ/Data/Configuration.cs

--- 5. LINQ/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-73NFD1L;Database=MusicHub;Trusted_Connection=True";
-    }
-}
